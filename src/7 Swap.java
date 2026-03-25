52f13fa9dbe086b6c10fa9e978dc43c1233d6e8d
SalviAjinkya <salviajinkya29@gmail.com>
2026-03-26 00:14:39 +0530

    learned swapping and need of temporary container

---
+++ src/7 Swap.java
@@ -0,0 +1,18 @@
+//create a program to swap two numbers
+
+import java.util.Scanner;
+
+class Swap {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to swapping Station");
+        System.out.println("Enter value of A: ");
+        int A = input.nextInt();
+        System.out.println("Enter value of B; ");
+        int B = input.nextInt();
+        int C = A;
+        A = B;
+        B = C;
+        System.out.println("Swapping of Numbers is done, now A=" + A + " and B=" + B);
+    }
+}
