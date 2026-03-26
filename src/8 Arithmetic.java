4fe204a33f3b39ac78149541eb2e90ec07d66253
SalviAjinkya <salviajinkya29@gmail.com>
2026-03-26 23:58:38 +0530

    Added mod to 8 Arithmetic

--- src/8 Arithmetic.java
+++ src/8 Arithmetic.java
@@ -4,7 +4,7 @@ class Arithmetic {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Give input of 2 numbers to get following operations solved: +, -, *, /");
+        System.out.println("Give input of 2 numbers to get following Arithmetic operations solved: +, -, *, /, %");
         System.out.println("Enter first number: ");
         int A = input.nextInt();
         System.out.println("Enter second number: ");
@@ -23,8 +23,15 @@ class Arithmetic {
         System.out.println("Multiplication of given numbers: " + mul);
 
         int div;
+        //below 2 lines are mistake
+        //int A = double A2; //here we converted our input int variable to double because to get answer in point
+        //int B = double B2; //in java we need both variable in double too not only ans
         div = A / B;
         System.out.println("Division of given numbers: " + div);
+
+        int mod;
+        mod = A % B;
+        System.out.println("Mod of given numbers: " + mod);
 
     }
 }
