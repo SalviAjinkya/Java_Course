fd710714a89cf78ff578796656259f455da49f97
SalviAjinkya <salviajinkya29@gmail.com>
2026-03-27 01:16:10 +0530

    13th problem: Compound Interest(pushing missing 13thproblem)

---
+++ src/13 CompoundInterest.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class CompoundInterest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to Compound Interest(CI) Calculator....");
+        System.out.println("To calculate CI we need Principal amount, Time is years and Rate of interest");
+        System.out.println("Please Enter Principal amount: ");
+        double P = input.nextDouble();
+        System.out.println("Please Enter, for how many years have you borrowed for(Time in years): ");
+        double T = input.nextDouble();
+        System.out.println("Please Enter Rate of Interest: ");
+        double R = input.nextDouble();
+
+        double CI = P * Math.pow((1+R/100), T);
+        System.out.println("Compound Interest is equal t: " + CI);
+    }
+}
