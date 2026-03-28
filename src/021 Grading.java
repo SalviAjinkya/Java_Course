1b70ce2f61479521330d9118ec8c973e186de7f7
SalviAjinkya <salviajinkya29@gmail.com>
2026-03-29 03:22:57 +0530

    021 grade calculator

---
+++ src/021 Grading.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Grading {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your percentage to know your grade: ");
+        int grade = input.nextInt();
+
+        if ( grade <=100 && grade>=0){
+            if (grade >= 90){
+                System.out.println("Your Grade is A");
+            } else if (grade >= 75){
+                System.out.println("Your Grade is B");
+            } else if(grade>=60){
+                System.out.println("Your Grade is C");
+            } else if(grade>=30){
+                System.out.println("Your Grade is D");
+            } else{
+                System.out.println("Your Grade is F");
+            }
+        }
+    }
+}
