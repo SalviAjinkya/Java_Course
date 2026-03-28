7be3d31142ae59ff4a88599d76884914d224f7b7
SalviAjinkya <salviajinkya29@gmail.com>
2026-03-29 02:38:26 +0530

    019 input part of problem

---
+++ src/019 GreatesstOfThreeNum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class GreatesstOfThreeNum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("We will help you find greatest of three given numbers...");
+        System.out.println("enter first number:");
+        int a = input.nextInt();
+        System.out.println("enter second number:");
+        int b = input.nextInt();
+        System.out.println("enter Third number:");
+        int c = input.nextInt();
+    }
+}
