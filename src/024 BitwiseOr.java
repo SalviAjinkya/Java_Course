c5fc1668fc56b2d7d1e88e325de51ef3400ee9d6
SalviAjinkya <salviajinkya29@gmail.com>
2026-03-29 17:55:53 +0530

    024 025 changes in title print, 026 starting part

--- src/024 BitwiseOr.java
+++ src/024 BitwiseOr.java
@@ -4,7 +4,7 @@ class BitwiseOr {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter two number to get its Bitwise And....");
+        System.out.println("Enter two number to get its Bitwise Or....");
         System.out.println("Enter first number: ");
         int a = input.nextInt();
         System.out.println("Enter second number: ");
---
+++ src/026 BitwiseCompliment.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class BitwiseCompliment {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter two number to get its Bitwise Compliment....");
+        System.out.println("Enter first number: ");
+        int a = input.nextInt();
+
+        int c = ;
+        System.out.println("Bitwise And of given two numbers is: " + c);
+    }
+}
