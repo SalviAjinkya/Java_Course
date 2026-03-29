0a0e35af20d9a7f53085c5b20dfe2b5d70dc4795
SalviAjinkya <salviajinkya29@gmail.com>
2026-03-29 18:07:51 +0530

    027 leftshift by input of user

--- src/027 BitwiseLeftShift.java
+++ src/027 BitwiseLeftShift.java
@@ -4,10 +4,13 @@ class BitwiseLeftShift {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter number to get its Bitwise Left Shift by 1:");
+        System.out.println("Welcome to Bitwise Left Shift calculator....");
+        System.out.println("By how much you want Left Shift?");
+        int s = input.nextInt();
+        System.out.println("Enter number to get its Bitwise Left Shift:");
         int a = input.nextInt();
 
-        int l = a << 1 ;
+        int l = a << s ;
         System.out.println("Bitwise And of given two numbers is: " + l);
     }
 }
