0622686a486aab4a5525d45f4f9b198aaae778c6
SalviAjinkya <salviajinkya29@gmail.com>
2026-03-31 23:58:46 +0530

    030 BitwisePowerOfTwo

---
+++ src/030 BitwisePowerOfTwo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class BitwisePowerOfTwo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number to check if it's a Power of Two: ");
+        int n = input.nextInt();
+
+
+        if (n > 0) {
+            int result = n & (n - 1);
+
+            if (result == 0) {
+                System.out.println(n + " is a Power of Two.");
+            } else {
+                System.out.println(n + " is NOT a Power of Two.");
+            }
+        } else {
+            System.out.println("Please enter a positive number greater than 0.");
+        }
+    }
+}
